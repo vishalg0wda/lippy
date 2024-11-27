6fd63b866c2ecd1c56564ac26ff752348213b4ac
Vishal Gowda <cartmanboy1991@gmail.com>
2024-11-27 00:03:13 +0000

    feat: initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::borrow::Cow;
+
+use owo_colors::OwoColorize;
+
+fn foo() {}
+
 fn main() {
-    println!("Hello, world!");
+    foo();
+    let a = 42u32;
+    let s = Cow::from(String::new());
+
+    let _r: &str = &s;
+    
+    println!("{a:>10}");
+    println!("what is going on here?!");
+
+    println!(
+        "{:<10}, {:}",
+        "Hello".bold(),
+        "world!".green().strikethrough()
+    );
 }
